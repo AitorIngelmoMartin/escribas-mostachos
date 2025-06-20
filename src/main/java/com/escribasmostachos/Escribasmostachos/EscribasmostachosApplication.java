9bb12e74651545af6784aa71233ce8127260a070
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-20 17:19:28 +0200

    first commit!

---
+++ src/main/java/com/escribasmostachos/Escribasmostachos/EscribasmostachosApplication.java
@@ -0,0 +1,13 @@
+package com.escribasmostachos.Escribasmostachos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscribasmostachosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscribasmostachosApplication.class, args);
+	}
+
+}
