dda0b2e7df9dea97a8209ace1afe6faaabcbd70a
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-20 19:01:25 +0200

    feature: created 'AuthResponse' DTO

---
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.escribasmostachos.Escribasmostachos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
