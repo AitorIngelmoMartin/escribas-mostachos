d0fe7609dcdf01cbc4921c866970ca410d6bcb64
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-20 18:42:20 +0200

    feature: created basic models

---
+++ src/main/java/com/escribasmostachos/Escribasmostachos/model/Book.java
@@ -0,0 +1,28 @@
+package com.escribasmostachos.Escribasmostachos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String coverUrl;
+
+    @ManyToOne
+    private User addedBy;
+}
