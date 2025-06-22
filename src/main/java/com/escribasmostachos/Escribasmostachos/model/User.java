50e92a26189db921d6d06c855677104b65727e6b
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 20:17:00 +0200

    feature: modified 'User' model to contains additional fields + implement 'toProfileDto' method

--- src/main/java/com/escribasmostachos/Escribasmostachos/model/User.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/model/User.java
@@ -1,18 +1,23 @@
 package com.escribasmostachos.Escribasmostachos.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.escribasmostachos.Escribasmostachos.dto.BookDTO;
 import com.escribasmostachos.Escribasmostachos.dto.RegisterRequestDto;
+import com.escribasmostachos.Escribasmostachos.dto.UserProfileDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -30,12 +35,26 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String username;
 
     private String password;
+
+    // Profile fields
+    private String firstName;
+    private String lastName;
+    private String profilePictureUrl;
+    // private List<String> favoriteGenres;
+    private Integer booksReadCount;
+
+    @OneToOne
+    private Book currentBook;
+
+    private LocalDate membershipDate;
+    // private List<String> roles;
+    private LocalDateTime lastActive;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -48,5 +67,29 @@ public class User implements UserDetails {
         user.setUsername(dto.getUsername());
         user.setPassword(encodedPassword);
         return user;
+    }
+
+    public UserProfileDto toProfileDto() {
+        UserProfileDto dto = new UserProfileDto();
+        dto.setUsername(this.username);
+        dto.setFirstName(this.firstName);
+        dto.setLastName(this.lastName);
+        dto.setProfilePictureUrl(this.profilePictureUrl);
+        // dto.setFavoriteGenres(this.favoriteGenres);
+        dto.setBooksReadCount(this.booksReadCount);
+
+        if (this.currentBook != null) {
+            BookDTO bookDTO = new BookDTO();
+            bookDTO.setTitle(this.currentBook.getTitle());
+            bookDTO.setAuthor(this.currentBook.getAuthor());
+            bookDTO.setCoverUrl(this.currentBook.getCoverUrl());
+            dto.setCurrentBook(bookDTO);
+        }
+
+        dto.setMembershipDate(this.membershipDate);
+        // dto.setRoles(this.roles);
+        dto.setLastActive(this.lastActive);
+
+        return dto;
     }
 }
