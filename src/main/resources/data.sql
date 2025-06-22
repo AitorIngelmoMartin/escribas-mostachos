276b4a5b8b3cbfe848f6bff660bdcb9a7dd5b624
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 12:22:38 +0200

    added local 'data.sql' to make local tests

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE users ( 
+--    id INT NOT NULL, 
+--    email VARCHAR(50) NOT NULL, 
+--    username VARCHAR(20), 
+--    password VARCHAR(20)
+-- );
+
+INSERT INTO users (id, email) VALUES (1, 'aitor@gmail.com');
