1691069d0fa3888b5d470e4e5b9cc8a7c0fb3f00
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 18:12:18 +0200

    feature: moved business logic from AuthController to a new class called 'AuthService'

--- src/main/java/com/escribasmostachos/Escribasmostachos/controller/AuthController.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/controller/AuthController.java
@@ -1,14 +1,7 @@
 package com.escribasmostachos.Escribasmostachos.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,43 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.escribasmostachos.Escribasmostachos.dto.ApiResponseDto;
 import com.escribasmostachos.Escribasmostachos.dto.LoginRequestDto;
 import com.escribasmostachos.Escribasmostachos.dto.RegisterRequestDto;
-import com.escribasmostachos.Escribasmostachos.model.User;
-import com.escribasmostachos.Escribasmostachos.repository.UserRepository;
-import com.escribasmostachos.Escribasmostachos.service.JwtService;
+import com.escribasmostachos.Escribasmostachos.service.AuthService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired
-    AuthenticationManager authenticationManager;
+    private final AuthService authService;
 
-    @Autowired
-    private final UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    private final JwtService jwtService;
-
-    public AuthController(JwtService jwtService, UserRepository userRepository) {
-        this.jwtService = jwtService;
-        this.userRepository = userRepository;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponseDto> registerUser(@RequestBody RegisterRequestDto registerRequest) {
-        log.info("Starting register operation");
-        if(userRepository.existsByEmail(registerRequest.getEmail())){
-            throw new DataIntegrityViolationException("Email " + registerRequest.getEmail() + " already used");
-        }
-
-        log.info("User received: " + registerRequest);
-        User newUser = User.fromDto(registerRequest, passwordEncoder.encode(registerRequest.getPassword()));
-        userRepository.save(newUser);
+        authService.register(registerRequest);
 
         return ResponseEntity
             .status(HttpStatus.OK)
@@ -63,16 +34,10 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponseDto> authenticateUser(@RequestBody LoginRequestDto loginRequest) {
-        log.info("Starting login operation");
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getEmail(),    
-                        loginRequest.getPassword()
-                )
-        );
-        User user = (User) authentication.getPrincipal();
+        String token = authService.login(loginRequest);
+
         return ResponseEntity
             .status(HttpStatus.OK)
-            .body(new ApiResponseDto(HttpStatus.OK, jwtService.generateToken(user)));
+            .body(new ApiResponseDto(HttpStatus.OK, token));
     }
 }
---
+++ src/main/java/com/escribasmostachos/Escribasmostachos/service/AuthService.java
@@ -0,0 +1,65 @@
+package com.escribasmostachos.Escribasmostachos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.escribasmostachos.Escribasmostachos.dto.LoginRequestDto;
+import com.escribasmostachos.Escribasmostachos.dto.RegisterRequestDto;
+import com.escribasmostachos.Escribasmostachos.model.User;
+import com.escribasmostachos.Escribasmostachos.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class AuthService {
+    
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager,
+                       JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    public void register(RegisterRequestDto registerRequest) {
+        log.info("Starting register operation");
+
+        if(userRepository.existsByEmail(registerRequest.getEmail())){
+            throw new DataIntegrityViolationException("Email " + registerRequest.getEmail() + " already used");
+        }
+
+        log.info("User received: " + registerRequest);
+        User newUser = User.fromDto(registerRequest, passwordEncoder.encode(registerRequest.getPassword()));
+        userRepository.save(newUser);
+    }
+
+    public String login(LoginRequestDto loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),    
+                        loginRequest.getPassword()
+                )
+        );
+        User user = (User) authentication.getPrincipal();
+        return jwtService.generateToken(user);
+    }
+}
