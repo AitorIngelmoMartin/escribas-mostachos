91df50379b158412d2d0b14f3ade5ea223cf294f
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 20:17:17 +0200

    added additional registry to data.sql to make local tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO users (email, username, password) VALUES ('a@b.com', 'username', '$2a$10$RjeE47YLF6vsbUDrJuL6.eRRTK5f7oS83824BflfBeJyXU6Jf39.y');
+INSERT INTO users (email, username, password, first_name, last_name) VALUES ('e@fcom', 'username', '$2a$10$RjeE47YLF6vsbUDrJuL6.eRRTK5f7oS83824BflfBeJyXU6Jf39.y','NombreUsuario','ApellidoUsuario');
