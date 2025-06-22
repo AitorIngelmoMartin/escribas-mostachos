2ad024ea7ea7dbef462775fbdc597369516d3bb5
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 13:57:41 +0200

    feature: modified class 'UserService' to implement 'UserDetailsService'

--- src/main/java/com/escribasmostachos/Escribasmostachos/service/UserService.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/service/UserService.java
@@ -2,16 +2,27 @@ package com.escribasmostachos.Escribasmostachos.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
 import com.escribasmostachos.Escribasmostachos.model.User;
 import com.escribasmostachos.Escribasmostachos.repository.UserRepository;
 
-@Service
-public class UserService {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Service
+public class UserService implements UserDetailsService {
+    
+    @Autowired
     private final UserRepository userRepository;
+    
     private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -19,15 +30,20 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public Optional<User> authenticate(String email, String rawPassword) {
-        Optional<User> userOptional = userRepository.findByEmail(email);
-        
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            if (passwordEncoder.matches(rawPassword, user.getPassword())) {
-                return Optional.of(user);
-            }
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("Finding user with email: " +  email);
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        if (!userOpt.isPresent())  {
+            throw new UsernameNotFoundException("User Not Found with email: " + email);
         }
-        return Optional.empty();
+        return userOpt.get();
+    }
+
+    public boolean authenticate(UserDetails user, String rawPassword) {
+        if (passwordEncoder.matches(rawPassword, user.getPassword())) {
+            return true;
+        }
+        return false;
     }
 }
