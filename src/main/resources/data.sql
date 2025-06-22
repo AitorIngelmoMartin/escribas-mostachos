4ae63af989a51835e64ba00403909675a59cc69b
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 17:11:53 +0200

    delete 'id' from data.sql first registry because that cause Data integrity error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (id, email, username, password) VALUES (1, 'a@b.com', 'username', '$2a$10$RjeE47YLF6vsbUDrJuL6.eRRTK5f7oS83824BflfBeJyXU6Jf39.y');
+INSERT INTO users (email, username, password) VALUES ('a@b.com', 'username', '$2a$10$RjeE47YLF6vsbUDrJuL6.eRRTK5f7oS83824BflfBeJyXU6Jf39.y');
