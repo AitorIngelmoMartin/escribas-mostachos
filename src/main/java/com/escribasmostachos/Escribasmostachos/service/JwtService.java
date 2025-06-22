8d458cda4e070453c466c7f8983d7064f0b88e6c
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-22 13:57:09 +0200

    feature: modified class 'JwtService' to use a '@PostConstruct' + refact: modified method 'isTokenValid' to use better logic

--- src/main/java/com/escribasmostachos/Escribasmostachos/service/JwtService.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/service/JwtService.java
@@ -2,6 +2,7 @@ package com.escribasmostachos.Escribasmostachos.service;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,12 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private final Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    private Key key;
+
+    @PostConstruct
+    public void initKey() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 
     public String generateToken(User user) {
         return Jwts.builder()
@@ -29,18 +35,33 @@ public class JwtService {
             .compact();
     }
 
-    public String extractEmail(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build()
-            .parseClaimsJws(token).getBody().getSubject();
-    }
-
     public boolean isTokenValid(String token, String email) {
-        return extractEmail(token).equals(email) && !isTokenExpired(token);
+        return getEmailFromToken(token).equals(email) && validateJwtToken(token);
     }
 
-    private boolean isTokenExpired(String token) {
-        Date expiration = Jwts.parserBuilder().setSigningKey(key).build()
-            .parseClaimsJws(token).getBody().getExpiration();
-        return expiration.before(new Date());
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            System.out.println("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("JWT token is expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("JWT token is unsupported: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty: " + e.getMessage());
+        }
+        return false;
     }
 }
