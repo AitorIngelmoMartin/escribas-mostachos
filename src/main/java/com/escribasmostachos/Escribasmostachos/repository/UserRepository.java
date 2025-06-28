52893a2423099ea7743aa2d5c56bb6017e99658b
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-28 11:38:44 +0200

    feature: added method 'findByEmailOrUsername' to 'UserRepository'

--- src/main/java/com/escribasmostachos/Escribasmostachos/repository/UserRepository.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/repository/UserRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByEmailOrUsername(String email, String username);
+
     boolean existsByEmail(String email);
 }
