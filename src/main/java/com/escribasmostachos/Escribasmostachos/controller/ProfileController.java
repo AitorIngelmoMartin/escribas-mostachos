d428b404852669c114c9f6979e0fc7d170427186
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-28 13:13:23 +0200

    feature: created /users/{username} endPoint on ProfileController

--- src/main/java/com/escribasmostachos/Escribasmostachos/controller/ProfileController.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/controller/ProfileController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,9 @@ import com.escribasmostachos.Escribasmostachos.dto.ApiResponseDto;
 import com.escribasmostachos.Escribasmostachos.dto.UserProfileDto;
 import com.escribasmostachos.Escribasmostachos.model.User;
 import com.escribasmostachos.Escribasmostachos.service.ProfileService;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/profile")
@@ -27,7 +31,18 @@ public class ProfileController {
     @GetMapping
     public ResponseEntity<ApiResponseDto<UserProfileDto>> getProfile(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
-        UserProfileDto userProfileDto = profileService.getProfile(user.getEmail());
+        UserProfileDto userProfileDto = profileService.getProfile(user.getUsername());
+        return ResponseEntity.ok(new ApiResponseDto<UserProfileDto>(HttpStatus.OK, "user profile obtained", userProfileDto));
+    }
+
+    @GetMapping("/users/{username}")
+    public ResponseEntity<ApiResponseDto<UserProfileDto>> getUserProfile(
+            @PathVariable
+            @Size(min = 3, max = 20, message = "Invalid username size")
+            @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Invalid username format")
+            String username) {
+        UserProfileDto userProfileDto = profileService.getProfile(username);
+
         return ResponseEntity.ok(new ApiResponseDto<UserProfileDto>(HttpStatus.OK, "user profile obtained", userProfileDto));
     }
 
