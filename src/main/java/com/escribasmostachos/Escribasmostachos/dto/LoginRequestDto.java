9384f1bd2aa346eb759500cad8928b25b3870616
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-28 11:36:01 +0200

    feature: added 'Pattern' to loguin and register DTOs + added username to register DTO

--- src/main/java/com/escribasmostachos/Escribasmostachos/dto/LoginRequestDto.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/LoginRequestDto.java
@@ -2,6 +2,7 @@ package com.escribasmostachos.Escribasmostachos.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,8 @@ public class LoginRequestDto {
 
     @NotBlank
     @Email
+    @Pattern(regexp = "^(?!.*\\.\\.)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$", 
+             message = "Invalid email format. Please enter a valid email without consecutive dots.")
     private String email;
 
     @NotBlank
--- src/main/java/com/escribasmostachos/Escribasmostachos/dto/RegisterRequestDto.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/RegisterRequestDto.java
@@ -1,6 +1,7 @@
 package com.escribasmostachos.Escribasmostachos.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Email;
 
 import lombok.Data;
@@ -9,9 +10,13 @@ import lombok.Data;
 public class RegisterRequestDto {
     @NotBlank
     @Email
+    @Pattern(regexp = "^(?!.*\\.\\.)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$", 
+             message = "Invalid email format. Please enter a valid email without consecutive dots.")
     private String email;
 
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$",
+             message = "Username can only contain letters, numbers, and underscore")
     private String username;
 
     @NotBlank
