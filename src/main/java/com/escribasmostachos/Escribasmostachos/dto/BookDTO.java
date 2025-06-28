501deb7ea4e71f169f1ea759ff91080953484304
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-28 17:59:03 +0200

    feature: added validation checks from jakarta to 'BookDto'

--- src/main/java/com/escribasmostachos/Escribasmostachos/dto/BookDTO.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/BookDTO.java
@@ -1,18 +1,24 @@
 package com.escribasmostachos.Escribasmostachos.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BookDTO {
     
-    @NotBlank
+    @NotBlank(message = "Title must not be blank")
+    @Size(max = 50, message = "Title must be at most 100 characters")
     private String title;
     
-    @NotBlank
+    @NotBlank(message = "Author must not be blank")
+    @Size(max = 30, message = "Author must be at most 60 characters")
     private String author;
     
+    @Pattern(regexp = "^(https?://.*)?$", message = "Cover URL must be a valid URL")
     private String coverUrl;
 
-    private UserProfileDto addedBy;
+    @Size(max = 50, message = "UpdatedBy must be at most 50 characters")
+    private String updatedBy;
 }
