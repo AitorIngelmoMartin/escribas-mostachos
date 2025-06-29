c2f4f46cacff2ae3ff570e899960da302e841d63
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-29 13:44:17 +0200

    feature: added 'IsbnValid' DTO validation to book dto with other generic jakarta validations

--- src/main/java/com/escribasmostachos/Escribasmostachos/dto/BookDTO.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/BookDTO.java
@@ -1,5 +1,7 @@
 package com.escribasmostachos.Escribasmostachos.dto;
 
+import com.escribasmostachos.Escribasmostachos.validations.IsbnValid;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,17 +10,21 @@ import lombok.Data;
 @Data
 public class BookDTO {
     
+    @NotBlank(message = "ISBN must not be blank")
+    @IsbnValid(message = "The ISBN must be valid ISBN-10 or ISBN-13")
+    private String isbn;
+
     @NotBlank(message = "Title must not be blank")
-    @Size(max = 50, message = "Title must be at most 100 characters")
+    @Size(max = 150, message = "Title cannot exceed 150 characters")
     private String title;
     
     @NotBlank(message = "Author must not be blank")
-    @Size(max = 30, message = "Author must be at most 60 characters")
+    @Size(max = 100, message = "Author cannot exceed 100 characters")
     private String author;
     
     @Pattern(regexp = "^(https?://.*)?$", message = "Cover URL must be a valid URL")
     private String coverUrl;
 
-    @Size(max = 50, message = "UpdatedBy must be at most 50 characters")
+    @Size(max = 20, message = "UpdatedBy must be a maximum of 20 characters")
     private String updatedBy;
 }
