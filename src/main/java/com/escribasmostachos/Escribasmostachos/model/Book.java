1fdc68ce01b05ca6b776d529fdc0c4fbb21e9782
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-29 13:43:42 +0200

    feature: added column validations in 'Book' model + added isbn and CoverUrl to DTO converter

--- src/main/java/com/escribasmostachos/Escribasmostachos/model/Book.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/model/Book.java
@@ -22,28 +22,34 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 20)
     private String isbn;
     
+    @Column(nullable = false, length = 150)
     private String title;
+
+    @Column(nullable = false, length = 100)
     private String author;
+    
     private String coverUrl;
     private String updatedBy;
 
     public Book fromDto(BookDTO dto) {
         Book book = new Book();
+        book.setIsbn(dto.getIsbn());
         book.setTitle(dto.getTitle());
         book.setAuthor(dto.getAuthor());
+        book.setCoverUrl(dto.getCoverUrl());
         return book;
     }
 
     public BookDTO toBookDto() {
         BookDTO dto = new BookDTO();
+        dto.setIsbn(this.isbn);
         dto.setTitle(this.title);
         dto.setAuthor(this.author);
         dto.setCoverUrl(this.coverUrl);
         dto.setUpdatedBy(this.updatedBy);
-
         return dto;
     }
 }
