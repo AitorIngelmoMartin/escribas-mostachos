7a46f1c7237fd745961fac040bbd1c8a3884060a
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-06-29 16:35:46 +0200

    fix: updated 'ProfileUpdateDto' to detect if the firstName or the lastName have at least 3 characters

--- src/main/java/com/escribasmostachos/Escribasmostachos/dto/ProfileUpdateDto.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/dto/ProfileUpdateDto.java
@@ -7,11 +7,11 @@ import lombok.Data;
 @Data
 public class ProfileUpdateDto {
 
-    @Size(min = 1, max = 25, message = "First name must be between 1 and 25 characters")
+    @Size(min = 3, max = 25, message = "First name must be between 3 and 25 characters")
     @Pattern(regexp = "^[\\p{L} .'-]+$", message = "invalid first name input")
     private String firstName;
 
-    @Size(min = 1, max = 25, message = "Last name must be between 1 and 25 characters")
+    @Size(min = 3, max = 25, message = "Last name must be between 3 and 25 characters")
     @Pattern(regexp = "^[\\p{L} .'-]+$", message = "invalid last name input")
     private String lastName;
 
