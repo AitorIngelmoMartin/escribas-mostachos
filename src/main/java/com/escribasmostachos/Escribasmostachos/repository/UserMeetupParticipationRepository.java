b8a645e39d4f334a7cbbffdb05b54047df6d3a04
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-07-27 19:25:50 +0200

    feat: added method deleteAllByMeetupId to UserMeetupParticipationRepository

--- src/main/java/com/escribasmostachos/Escribasmostachos/repository/UserMeetupParticipationRepository.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/repository/UserMeetupParticipationRepository.java
@@ -2,7 +2,11 @@ package com.escribasmostachos.Escribasmostachos.repository;
 
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.escribasmostachos.Escribasmostachos.model.ReadingMeetup;
 import com.escribasmostachos.Escribasmostachos.model.User;
@@ -16,4 +20,9 @@ public interface UserMeetupParticipationRepository extends JpaRepository<UserMee
     Optional<UserMeetupParticipation> findByMeetupIdAndUserId(Long meetupId, Long userId);
     
     void deleteByMeetupIdAndUserId(Long meetupId, Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM UserMeetupParticipation ump WHERE ump.meetup.id = :meetupId")
+    void deleteAllByMeetupId(@Param("meetupId") Long meetupId);
 }
