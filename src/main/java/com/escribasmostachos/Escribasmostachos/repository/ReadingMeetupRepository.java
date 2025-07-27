618f79b15982915a6c56ec6e464ef834aed786f7
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-07-27 19:05:37 +0200

    feat: modified 'ReadingMeetupRepository' to have custom querys to optimize database operations

--- src/main/java/com/escribasmostachos/Escribasmostachos/repository/ReadingMeetupRepository.java
+++ src/main/java/com/escribasmostachos/Escribasmostachos/repository/ReadingMeetupRepository.java
@@ -3,6 +3,8 @@ package com.escribasmostachos.Escribasmostachos.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.escribasmostachos.Escribasmostachos.model.MeetupStatus;
@@ -15,5 +17,26 @@ public interface ReadingMeetupRepository extends JpaRepository<ReadingMeetup, Lo
     Page<ReadingMeetup> findAllByStatus(MeetupStatus status, Pageable pageable);
     Page<ReadingMeetup> findAllByCreatorId(Long creatorId, Pageable pageable);
     Page<ReadingMeetup> findAllByStatusAndCreatorId(MeetupStatus status, Long creatorId, Pageable pageable);
-    Optional<ReadingMeetup> findByCreatorIdAndBookIdAndStatus(Long creatorId, Long bookId, MeetupStatus excludedStatus);
+    boolean existsByCreatorIdAndBookIdAndStatus(Long creatorId, Long bookId, MeetupStatus status);
+
+    @Query("""
+    SELECT rm FROM ReadingMeetup rm
+    JOIN FETCH rm.creator
+    JOIN FETCH rm.book
+    LEFT JOIN FETCH rm.participants
+    WHERE rm.id = :id
+    """)
+    Optional<ReadingMeetup> findByIdWithDetails(@Param("id") Long id);
+
+    @Query("""
+        SELECT rm FROM ReadingMeetup rm
+        JOIN FETCH rm.creator
+        JOIN FETCH rm.book
+        WHERE (:status IS NULL OR rm.status = :status)
+        AND (:creatorId IS NULL OR rm.creator.id = :creatorId)
+    """)
+    Page<ReadingMeetup> findWithDetails(
+        @Param("status") MeetupStatus status,
+        @Param("creatorId") Long creatorId,
+        Pageable pageable);
 }
