90c30d6029c8d794c3c2d21936b80a96c1cbcea8
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2025-07-27 19:02:56 +0200

    refact: replaced 'BusinessConflictException' with 'BusinessLogicalException'

--- src/main/java/com/escribasmostachos/Escribasmostachos/exception/BusinessConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.escribasmostachos.Escribasmostachos.exception;
-
-public class BusinessConflictException extends RuntimeException {
-    public BusinessConflictException(String message) {
-        super(message);
-    }
-}
